baf307995fc9bce9b69637d401e3ab1f15fd0556
Alan Gomez <soyalangg@gmail.com>
2023-06-24 01:59:39 -0600

    eventhub auth

---
+++ customuser/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from dj_rest_auth.serializers import UserDetailsSerializer
+from dj_rest_auth.serializers import LoginSerializer
+
+class CustomRegisterSerializer(RegisterSerializer):
+    full_name = serializers.CharField()
+    is_provider = serializers.BooleanField()
+
+    def get_cleaned_data(self):
+        super(CustomRegisterSerializer, self).get_cleaned_data()
+        return {
+            'username': self.validated_data.get('username', ''),
+            'full_name': self.validated_data.get('full_name', ''),
+            'email': self.validated_data.get('email', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'password2': self.validated_data.get('password2', ''),
+            'is_provider': self.validated_data.get('is_provider', ''),
+        }
+    
+class CustomUserDetailsSerializer(UserDetailsSerializer):
+    class Meta(UserDetailsSerializer.Meta):
+        fields = ('pk','username','email','full_name','is_provider')
+        read_only_fields = ('username','email','first_name','last_name')
+
+class CustomLoginSerializer(LoginSerializer):
+    username = None
---
+++ customuser/urls.py
@@ -0,0 +1,7 @@
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('dj_rest_auth.urls')),
+    path('register/', include('dj_rest_auth.registration.urls')),
+    path('accounts/', include('allauth.urls'))
+]
