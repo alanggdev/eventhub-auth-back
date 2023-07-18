6ef49fb168979de63d570efaa3373a30f03799d8
Alan Gomez <soyalangg@gmail.com>
2023-07-17 19:14:19 -0600

    fix: firebase_toke nulleable

--- customuser/models.py
+++ customuser/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class CustomUser(AbstractUser):
     full_name = models.CharField(null=True, max_length=50)
     is_provider = models.BooleanField(null=True)
-    firebase_token = models.TextField(null=True)
+    firebase_token = models.TextField(null=True, blank=True)
